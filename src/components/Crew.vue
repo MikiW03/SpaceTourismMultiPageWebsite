19b0b12f34e8373e296703292cf0ec08a568f0d7
MikiW03 <mikiwiniecki@gmail.com>
2022-07-15 16:40:05 +0200

    Add vue router and basing styling

---
+++ src/components/Crew.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="content">
+        <h1>Crew</h1>
+    </div>
+</template>
+
+<style scoped>
+.content {
+    background-image: url("../assets/crew/background-crew-desktop.jpg");
+}
+</style>
---
+++ src/components/Destination.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="content">
+        <h1>Destination</h1>
+    </div>
+</template>
+
+<style scoped>
+.content {
+    background-image: url("../assets/destination/background-destination-desktop.jpg");
+}
+</style>
