b807a149456816bdccff0cf5c6add132cacadac1
MikiW03 <mikiwiniecki@gmail.com>
2022-07-18 16:28:32 +0200

    Home site

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,10 +4,156 @@
 
 <template>
     <div class="background"></div>
+    <section>
+        <article>
+            <h5>SO, YOU WANT TO TRAVEL TO</h5>
+            <h1>SPACE</h1>
+            <p>
+                Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover
+                kind
+                of
+                on
+                the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
+            </p>
+        </article>
+        <button>
+            <span>EXPLORE</span>
+        </button>
+    </section>
 </template>
 
 <style scoped>
 .background {
-    background-image: url("../assets/home/background-home-desktop.jpg");
+    background-image: url("../assets/home/background-home-mobile.jpg");
+}
+
+section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    gap: 81px;
+}
+
+article {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 16px;
+
+    margin-top: 48px;
+}
+
+h5 {
+    font-size: 16px;
+}
+
+h1 {
+    font-size: 80px;
+    color: var(--accent-color)
+}
+
+p {
+    font-size: 15px;
+    text-align: center;
+    letter-spacing: 0;
+
+    margin-inline: 24px;
+    max-width: 444px;
+}
+
+button {
+    border: 0;
+    border-radius: 50%;
+
+    width: 150px;
+    aspect-ratio: 1;
+
+    font-family: "Bellefair";
+    font-size: 20px;
+    letter-spacing: 1.25px;
+}
+
+@media (min-width: 35.05rem) and (max-width: 55rem) {
+    .background {
+        background-image: url("../assets/home/background-home-tablet.jpg");
+    }
+
+    section {
+        gap: 156px;
+    }
+
+    article {
+        gap: 24px;
+        margin-top: 106px;
+    }
+
+    h5 {
+        font-size: 20px;
+    }
+
+    h1 {
+        font-size: 150px;
+    }
+
+    p {
+        font-size: 16px;
+    }
+
+    button {
+        width: 242px;
+
+        font-size: 32px;
+        letter-spacing: 2px;
+    }
+}
+
+@media (min-width: 55rem) {
+    .background {
+        background-image: url("../assets/home/background-home-desktop.jpg");
+    }
+
+    section {
+        flex-direction: row;
+        gap: 396px;
+        justify-content: center;
+        align-items: flex-end;
+        margin-top: 251px;
+        margin-inline: 100px;
+    }
+
+    article {
+        align-items: flex-start;
+        margin-top: 0;
+        gap: 24px;
+    }
+
+    h5 {
+        font-size: clamp(20px, 2vw, 28px);
+        letter-spacing: 4.72px;
+    }
+
+    h1 {
+        font-size: clamp(100px, 10.5vw, 150px);
+    }
+
+    p {
+        font-size: 18px;
+
+        margin-inline: 0;
+        text-align: left;
+    }
+
+    button {
+        width: 274px;
+        font-size: 32px;
+        letter-spacing: 2px;
+    }
+}
+
+@media (min-width: 32.05rem) and (max-width: 1320px) {
+    section {
+        gap: clamp(15px, 7vw, 100px);
+    }
 }
 </style>
